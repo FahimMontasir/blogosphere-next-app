565b2f490ff9a90db04efeec100d53813ca7b951
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-09-05 07:46:21 +0600

    add: tailwindcss for better managing styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,18 +13,21 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="font-bold underline text-green-300">hello fahim</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2 className="text-3xl font-bold underline">
+              Documentation &rarr;
+            </h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
@@ -59,14 +62,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ pages/yoyo.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+
+const Yoyo: NextPage = () => {
+  return <h1>hello</h1>;
+};
+export default Yoyo;
